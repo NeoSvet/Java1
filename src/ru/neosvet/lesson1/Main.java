f3e2c45e2ffd9d848ee3442bddfde18025ed624a
Neosvet <neosvet333@gmail.com>
2020-10-07 08:52:27 +0300

    lesson 1 task 2

--- src/ru/neosvet/lesson1/Main.java
+++ src/ru/neosvet/lesson1/Main.java
@@ -3,6 +3,17 @@ package ru.neosvet.lesson1;
 public class Main {
 
     public static void main(String[] args) {
+        initVals();
+    }
 
+    private static void initVals() {
+        byte byteVal = 4;
+        short shortVal = 1000;
+        int intVal = 99110;
+        long longVal = 1010101L;
+        float floatVal = 124.34f;
+        double doubleVal = 33334.1313131;
+        char charVal = 'C';
+        boolean booleanVal = true;
     }
 }
