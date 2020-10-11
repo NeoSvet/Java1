e5d2cd1e0b49729f121c3d86979976fc05516e13
Neosvet <neosvet333@gmail.com>
2020-10-11 13:06:48 +0300

    lesson 2 task 1

--- src/ru/neosvet/lesson2/Main.java
+++ src/ru/neosvet/lesson2/Main.java
@@ -1,8 +1,19 @@
 package ru.neosvet.lesson2;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] task1 = invertArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0});
+        System.out.println("Result of task 1: " + Arrays.toString(task1));
+    }
 
+    private static int[] invertArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0 || arr[i] == 1)
+                arr[i] = (arr[i] == 0 ? 1 : 0);
+        }
+        return arr;
     }
 }
