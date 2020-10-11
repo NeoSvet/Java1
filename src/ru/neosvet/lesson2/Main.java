3b50a89e4da8f5b94f9128f74106e6ddcef7b47b
Neosvet <neosvet333@gmail.com>
2020-10-11 13:21:48 +0300

    lesson 2 task 4

--- src/ru/neosvet/lesson2/Main.java
+++ src/ru/neosvet/lesson2/Main.java
@@ -9,13 +9,24 @@ public class Main {
         System.out.println("Result of task 1: " + Arrays.toString(result));
         result = getNumsMultiplesThree(8);
         System.out.println("Result of task 2: " + Arrays.toString(result));
-        result = increaseNums(new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}, 6);
+        result = increaseNums(new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}, 6);
         System.out.println("Result of task 3: " + Arrays.toString(result));
+        printMatrixCross(5);
+    }
+
+    private static void printMatrixCross(int len) {
+        int[][] arr = new int[len][len];
+        System.out.println("Result of task 4:");
+        for (int i = 0; i < len; i++) {
+            arr[i][i] = 1;
+            arr[i][len - i - 1] = 1;
+            System.out.println(Arrays.toString(arr[i]));
+        }
     }
 
     private static int[] increaseNums(int[] nums, int lessThan) {
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] < lessThan)
+            if (nums[i] < lessThan)
                 nums[i] *= 2;
         }
         return nums;
