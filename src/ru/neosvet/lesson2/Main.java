0eac9525e164bd87e9a26145ad0989a46eec02df
Neosvet <neosvet333@gmail.com>
2020-10-11 13:07:06 +0300

    lesson 2 task 2

--- src/ru/neosvet/lesson2/Main.java
+++ src/ru/neosvet/lesson2/Main.java
@@ -7,6 +7,16 @@ public class Main {
     public static void main(String[] args) {
         int[] task1 = invertArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0});
         System.out.println("Result of task 1: " + Arrays.toString(task1));
+        int[] task2 = getNumsMultiplesThree(8);
+        System.out.println("Result of task 2: " + Arrays.toString(task2));
+    }
+
+    private static int[] getNumsMultiplesThree(int len) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = i * 3;
+        }
+        return arr;
     }
 
     private static int[] invertArray(int[] arr) {
