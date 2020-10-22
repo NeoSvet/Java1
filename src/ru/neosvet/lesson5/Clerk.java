b76a00d4d0916fe63e28724901c76ce99bee3085
Neosvet <neosvet333@gmail.com>
2020-10-22 13:06:42 +0300

    created constructor

--- src/ru/neosvet/lesson5/Clerk.java
+++ src/ru/neosvet/lesson5/Clerk.java
@@ -3,5 +3,14 @@ package ru.neosvet.lesson5;
 public class Clerk {
     private String name, position, email, phone;
     private int salary, age;
+
+    public Clerk(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 }
 
