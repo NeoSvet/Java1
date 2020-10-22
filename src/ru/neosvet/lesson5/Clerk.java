bc95357fbeb38a5018bfc58098bb93d08f2d7620
Neosvet <neosvet333@gmail.com>
2020-10-22 13:14:12 +0300

    created print

--- src/ru/neosvet/lesson5/Clerk.java
+++ src/ru/neosvet/lesson5/Clerk.java
@@ -12,5 +12,10 @@ public class Clerk {
         this.salary = salary;
         this.age = age;
     }
+
+    public void print() {
+        System.out.printf("Сотрудник: %s, должность: %s, email: %s, телефон: %s, " +
+                "зарплата: %,d, возраст: %d.%n", name, position, email, phone, salary, age);
+    }
 }
 
