40ede42ecf161b076d020cc230bf04f9b101de44
Neosvet <neosvet333@gmail.com>
2020-10-29 22:03:13 +0300

    created startObstacles

--- src/ru/neosvet/lesson6/Main.java
+++ src/ru/neosvet/lesson6/Main.java
@@ -1,7 +1,34 @@
 package ru.neosvet.lesson6;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+    private static final int maxRun = 600, maxSwim = 14, maxJump = 3;
+    private static final int minRun = 50, minSwim = 3;
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Бобик");
+        Cat cat = new Cat("Мурка");
+
+        startObstacles(new Animal[]{dog, cat});
+    }
+
+    private static void startObstacles(Animal[] animals) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int run, swim, i;
+        float jump;
+        do {
+            run = random.nextInt(maxRun - minRun) + minRun;
+            swim = random.nextInt(maxSwim - minSwim) + minSwim;
+            jump = (float) random.nextInt(maxJump) + random.nextFloat();
+            for (i = 0; i < animals.length; i++) {
+                animals[i].run(run);
+                animals[i].swim(swim);
+                animals[i].jump(jump);
+            }
+            System.out.println("Продолжить мучать зверушек? 1 – да / другое - закончить");
+        } while (scanner.next().equals("1"));
     }
 }
