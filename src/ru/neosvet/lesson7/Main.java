f63d002522b75ddd3cca0df23868537546a6c43e
Neosvet <neosvet333@gmail.com>
2020-10-31 21:17:44 +0300

    first realize game

--- src/ru/neosvet/lesson7/Main.java
+++ src/ru/neosvet/lesson7/Main.java
@@ -1,26 +1,41 @@
 package ru.neosvet.lesson7;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static final int MAX_APPETITE = 4;
+    private static final int CAPACITY = 10;
 
     public static void main(String[] args) {
         startGame(getCats(), getPlate());
     }
 
     private static void startGame(Cat[] cats, Plate plate) {
-        plate.addFood(6);
-        plate.printInfo();
-        for (int i = 0; i < cats.length; i++) {
-            cats[i].eat(plate);
-        }
-        plate.printInfo();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int day = 1;
+
+        do {
+            System.out.println("День №" + day);
+            plate.addFood(random.nextInt(plate.getCapacity()));
+            plate.printInfo();
+            for (int i = 0; i < cats.length; i++) {
+                cats[i].eat(plate);
+                cats[i].addAppetite(random.nextInt(MAX_APPETITE));
+            }
+            plate.printInfo();
+            day++;
+            System.out.println("1 – продолжить / другое - закончить");
+        } while (scanner.next().equals("1"));
     }
 
     private static Plate getPlate() {
-        return new Plate(10);
+        return new Plate(CAPACITY);
     }
 
     private static Cat[] getCats() {
-        return new Cat[] {
+        return new Cat[]{
                 new Cat("Борис Николаевич", 3),
                 new Cat("Мурзик", 1),
                 new Cat("Юкуб", 2)
