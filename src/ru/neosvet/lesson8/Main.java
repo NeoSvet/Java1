7c51da0cfac89fdfb575a323186702fabefd559d
Neosvet <neosvet333@gmail.com>
2020-11-05 09:39:08 +0300

    realized game

--- src/ru/neosvet/lesson8/Main.java
+++ src/ru/neosvet/lesson8/Main.java
@@ -1,14 +1,22 @@
 package ru.neosvet.lesson8;
 
+import ru.neosvet.lesson4.Coords;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main extends JFrame {
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Main extends JFrame implements ActionListener {
     public static void main(String[] args) {
         new Main();
     }
+
+    private final int SIZE = 5;
+    private TicTacToe game;
+    private JPanel field;
 
     public Main() {
         setTitle("TicTacToe");
@@ -23,11 +31,60 @@ public class Main extends JFrame {
     }
 
     private void createField() {
-        JPanel panel = new JPanel();
-        add(panel, BorderLayout.CENTER);
-        panel.setLayout(new GridLayout(5, 5));
-        for (int i = 0; i < 25; i++)
-            panel.add(createButton());
+        field = new JPanel();
+        add(field, BorderLayout.CENTER);
+        field.setLayout(new GridLayout(SIZE, SIZE));
+        Font font = new Font(field.getFont().getName(),
+                field.getFont().getStyle(),
+                field.getFont().getSize() + 60);
+
+        Cell cell;
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                cell = new Cell(x, y, font);
+                cell.addActionListener(this);
+                field.add(cell);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (game == null || game.isFinish()) {
+            showMessageDialog(this, "Начните новую игру!");
+            return;
+        }
+        Cell cell = (Cell) e.getSource();
+        if (game.isBusyCell(cell.getCoords()))
+            return;
+        game.goHumanTurn(cell.getCoords());
+        cell.setText(game.getText(true));
+        if(game.isFinish()) {
+            showFinishMessage();
+        } else {
+            goAiTurn();
+        }
+    }
+
+    private void showFinishMessage() {
+        showMessageDialog(this, game.getMsgFinish());
+    }
+
+    private void goAiTurn() {
+        Coords coords = game.getCoordsAiTurn();
+        Cell cell;
+        for (int i = 0; i < field.getComponentCount(); i++) {
+            cell = (Cell) field.getComponent(i);
+            if(coords.getX() == cell.getCoords().getX() &&
+                    coords.getY() == cell.getCoords().getY()) {
+                cell.setText(game.getText(false));
+                break;
+            }
+        }
+
+        if(game.isFinish()) {
+            showFinishMessage();
+        }
     }
 
     private void createMenu() {
@@ -37,7 +94,7 @@ public class Main extends JFrame {
         first.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO start game (human as first)
+                startGame(true);
             }
         });
         game.add(first);
@@ -45,7 +102,7 @@ public class Main extends JFrame {
         second.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //TODO start game (human as second)
+                startGame(false);
             }
         });
         game.add(second);
@@ -53,14 +110,15 @@ public class Main extends JFrame {
         add(menu, BorderLayout.PAGE_START);
     }
 
-    private Component createButton() {
-        JButton button = new JButton();
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO human turn
+    private void startGame(boolean human_first) {
+        game = new TicTacToe(human_first);
+        char[][] map = game.getMap();
+        Cell cell;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                cell = (Cell) field.getComponent(i * SIZE + j);
+                cell.setText(String.valueOf(map[i][j]));
             }
-        });
-        return button;
+        }
     }
 }
