19db4c0552a8d2f98a9b67c64cc02074f8eac43b
Neosvet <neosvet333@gmail.com>
2020-11-05 08:22:29 +0300

    created GUI

--- src/ru/neosvet/lesson8/Main.java
+++ src/ru/neosvet/lesson8/Main.java
@@ -1,7 +1,66 @@
 package ru.neosvet.lesson8;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        setTitle("TicTacToe");
+        setSize(500, 530);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        createMenu();
+        createField();
+
+        setVisible(true);
+    }
+
+    private void createField() {
+        JPanel panel = new JPanel();
+        add(panel, BorderLayout.CENTER);
+        panel.setLayout(new GridLayout(5, 5));
+        for (int i = 0; i < 25; i++)
+            panel.add(createButton());
+    }
+
+    private void createMenu() {
+        JMenuBar menu = new JMenuBar();
+        JMenu game = new JMenu("Игра");
+        JMenuItem first = new JMenuItem("Начать первым");
+        first.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO start game (human as first)
+            }
+        });
+        game.add(first);
+        JMenuItem second = new JMenuItem("Начать вторым");
+        second.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO start game (human as second)
+            }
+        });
+        game.add(second);
+        menu.add(game);
+        add(menu, BorderLayout.PAGE_START);
+    }
+
+    private Component createButton() {
+        JButton button = new JButton();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO human turn
+            }
+        });
+        return button;
     }
 }
